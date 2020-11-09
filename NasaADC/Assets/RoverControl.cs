44996a0bbfbe9b08168a146ad01bb5709e8e18dd
samuellihn <68252237+samuellihn@users.noreply.github.com>
2020-11-08 22:05:37 -0500

    switched to new control system, fixed some bugs

--- NasaADC/Assets/RoverControl.cs
+++ NasaADC/Assets/RoverControl.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public enum Axle {
     Front,
@@ -13,16 +14,22 @@ public struct Wheel {
     public GameObject model;
     public WheelCollider collider;
     public Axle axle;
+    public bool brake;
 }
 
 
 public class RoverControl : MonoBehaviour {
-    public float movementForce = 500f;
+
+
+    public float movementForce = 1000f;
+    public float brakeForce = 1000f;
+    public float brakeSpeed = 500f;
+    public Vector3 roverCenterOfMass;
 
     [SerializeField]
-    private float maxAcceleration = 20.0f;
+    private float maxAcceleration = 200.0f;
     [SerializeField]
-    private float keyboardTurnSensitivity = 0.1f;
+    private float keyboardTurnSensitivity = 0.3f;
     [SerializeField]
     private float maxSteerAngle = 45.0f;
     [SerializeField]
@@ -33,69 +40,72 @@ public class RoverControl : MonoBehaviour {
     private Rigidbody rb;
     private float currentAngle;
     private Vector3 wheelModelOffset;
+    private float currentBrakeForce;
+    private float thrustPower;
+    private float turnPower;
+
+    public DefaultControl controls;
     
     // Start is called before the first frame update
+    
+    private void Awake() {
+        controls = new DefaultControl();
+        controls.Player.Throttle.performed += throt => thrustPower = (throt.ReadValue<float>());
+        controls.Player.Steer.performed += str => turnPower = (str.ReadValue<float>());
+    }
+
+    private void OnEnable() {
+        controls.Enable();
+    }
+
+    private void OnDisable() {
+        controls.Disable();
+    }
+
     private void Start() {
         wheelModelOffset = wheels[0].model.transform.position - wheels[0].collider.transform.position;
     }
 
     // Update is called once per frame
     private void Update() {
-        GetInputs();
     }
 
-    private void GetInputs() {
-        // Keyboard input WASD
-        if (Input.GetKey("w")) {
-            inputY = 1f;
-        }
-        else if (Input.GetKey("s")) {
-            inputY = -1f;
-        }
-        else {
-            inputY = 0;
-        }
-        if (Input.GetKey("a")) {
-            inputX = -1f;
-        }
-        else if (Input.GetKey("d")) {
-            inputX = 1f;
-        }
-        else {
-            inputX = 0;
-        }
-        // Debug.Log(inputY);
-        // Debug.Log(inputX);
-    }
-
-    private void FixedUpdate()
-    {
+    private void FixedUpdate() {
         Move();
         Turn();
         AnimateWheels();
     }
 
     private void Move() {
-        // Apply torque to wheels if acceleration is held down
-        foreach (var wheel in wheels) {
-            wheel.collider.motorTorque = inputY * maxAcceleration * movementForce * Time.deltaTime;
-
+        if (thrustPower !=0f) {
+            // Power all wheels
+            foreach (var wheel in wheels) {
+                wheel.collider.brakeTorque = 0f;
+                wheel.collider.motorTorque = thrustPower * maxAcceleration * movementForce * Time.deltaTime;
+                // Debug.Log(wheel.collider.motorTorque);
+                // Debug.Log(wheel.collider.brakeTorque);
+            }
+        }
+        else {
+            // Brake all wheels
+            foreach (var wheel in wheels) {
+                if (wheel.brake) {
+                    if (wheel.collider.brakeTorque < brakeForce) {
+                        wheel.collider.brakeTorque += brakeSpeed * Time.deltaTime;
+                        // Debug.Log(wheel.collider.brakeTorque);
+                    }
+                }
+            }
         }
     }
 
     private void Turn() {
         foreach (var wheel in wheels) {
             if (wheel.axle == Axle.Front) {
-
-                // Keyboard Input Specific Code
-                // Is turn held down?
-                // Yes, start turning
-                if (inputX != 0f) {
-                    currentAngle += Time.deltaTime * keyboardTurnSensitivity * inputX;
-                }
-                // No, return to center
-                else {
-                    currentAngle -= Time.deltaTime * keyboardTurnSensitivity / 0.5f * Mathf.Sign(wheel.collider.steerAngle);
+                currentAngle = Mathf.Clamp(currentAngle + Time.deltaTime * keyboardTurnSensitivity * turnPower, -1, 1);
+                Debug.Log(currentAngle);
+                if (turnPower == 0f) {
+                    currentAngle -= Time.deltaTime * keyboardTurnSensitivity / 2f * Mathf.Sign(wheel.collider.steerAngle);
                 }
                 wheel.collider.steerAngle = Mathf.Lerp(-maxSteerAngle, maxSteerAngle, currentAngle / 2f + 0.5f);
             }
