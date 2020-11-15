e135f3b6dd46010ca270c65d9a9251d112f37e26
samuellihn <68252237+samuellihn@users.noreply.github.com>
2020-11-14 21:17:57 -0500

    tweaked some driving parameters

--- NasaADC/Assets/CameraControl.cs
+++ NasaADC/Assets/CameraControl.cs
@@ -10,6 +10,7 @@ public class CameraControl : MonoBehaviour
     public Transform rover;
     public Vector3 cameraOffset;
     public DefaultControl controls;
+    public float smoothAmount;
     
     private bool lookMode;
     // Start is called before the first frame update
@@ -30,7 +31,7 @@ public class CameraControl : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate() {
         Vector3 desiredPosition = rover.TransformPoint(cameraOffset);
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, 0.125f);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothAmount);
         transform.position = smoothedPosition;
         transform.eulerAngles = rover.rotation.eulerAngles;
 
