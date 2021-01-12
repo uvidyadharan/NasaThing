3bba1766d127f8086cc5b85e7aa4e9ea35b5222a
adimachinelearning <adimachinelearning@gmail.com>
2021-01-12 12:50:14 -0500

    Cleaned and organized

---
+++ NasaADC-Clean/Assets/UI/MinimapCamControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinimapCamControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform rover;
+    public Vector3 minimapCameraOffset;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = rover.position + minimapCameraOffset;
+        transform.eulerAngles = new Vector3(90,0, -rover.rotation.eulerAngles.y);
+    }
+}
