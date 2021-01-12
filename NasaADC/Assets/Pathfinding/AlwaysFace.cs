ea212a01f0d228f6a2bfe3a87ce4b8daf1fb9c41
samuellihn <samuellihn@gmail.com>
2021-01-12 10:41:36 -0500

    some organization

--- NasaADC/Assets/Pathfinding/AlwaysFace.cs
+++ NasaADC/Assets/Pathfinding/AlwaysFace.cs
@@ -2,14 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class alwaysFace : MonoBehaviour
+public class AlwaysFace : MonoBehaviour
 {
     public Transform lookingAt;
-    // Update is called once per frame
-    void Start()
-    {
-
-    }
     void Update()
     {
         this.transform.rotation = Quaternion.LookRotation(this.transform.position - lookingAt.position);
