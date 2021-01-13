5dc03a6e8329e3858d5acfab4d69c0b65f1d0d8b
samuellihn <samuellihn@gmail.com>
2021-01-13 16:13:07 -0500

    HDRP inital

---
+++ NasaADC/Assets/Intro/PlayIntro.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PlayIntro : MonoBehaviour {
+
+    private string movie = "LoadingScreenAnimation.mov";
+
+    void Start () 
+    {
+        StartCoroutine(streamVideo(movie));
+    }
+
+    private IEnumerator streamVideo(string video)
+    {
+        Handheld.PlayFullScreenMovie(video, Color.black, FullScreenMovieControlMode.Hidden, FullScreenMovieScalingMode.Fill);
+        yield return new WaitForEndOfFrame ();
+        SceneManager.LoadScene ("MainScene");
+    }
+}
---
+++ NasaADC/Assets/SceneLoad.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadAsyncScene());
+    }
+
+    IEnumerator LoadAsyncScene()
+    {
+        float loadTime = Time.time;
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("MainScene");
+        asyncLoad.allowSceneActivation = false;
+        while(asyncLoad.progress <= 0.89f){
+               yield return null;
+        }
+
+        loadTime = Time.time - loadTime;
+        loadTime %= 6;
+
+        yield return new WaitForSeconds(6.2f-loadTime);
+
+        asyncLoad.allowSceneActivation = true;
+    }
+
+}
