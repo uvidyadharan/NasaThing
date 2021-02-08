1c275222cb514972bc9c740e0bfc2823db12b17c
Reva Amritkar <revamamritkar@gmail.com>
2021-02-08 17:57:24 -0500

    main menu

---
+++ NasaADC/Assets/Scenes/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void GoBack()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void OptionsPage()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void CreditsPage()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ControlsPage()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+}
