b282cd23532c59b4a62aa177d78c6f4704a48fe9
Reva Amritkar <revamamritkar@gmail.com>
2021-02-15 19:51:50 -0500

    Fixed Buttons

---
+++ NasaADC/Assets/Scenes/VolumeSlider.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Music : MonoBehaviour
+{
+    public AudioClip[] soundtrack;
+    public Slider volumeSlider;
+
+    AudioSource audioSource;
+
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        if (!audioSource.playOnAwake)
+        {
+            audioSource.clip = soundtrack[Random.Range(0, soundtrack.Length)];
+            audioSource.Play();
+        }
+    }
+
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = soundtrack[Random.Range(0, soundtrack.Length)];
+            audioSource.Play();
+        }
+    }
+
+    void OnEnable()
+    {
+        volumeSlider.onValueChanged.AddListener(delegate { changeVolume(volumeSlider.value); });
+    }
+
+    void changeVolume(float sliderValue)
+    {
+        audioSource.volume = sliderValue;
+    }
+
+    void OnDisable()
+    {
+        volumeSlider.onValueChanged.RemoveAllListeners();
+    }
+}
